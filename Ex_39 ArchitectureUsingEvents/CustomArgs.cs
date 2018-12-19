71b01e07176fc8757a4e037797adc5ae557eebef
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2018-12-19 08:38:46 +0100

    start

---
+++ Ex_39 ArchitectureUsingEvents/CustomArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelViewPresenter01
+{
+    public class CustomArgs : EventArgs
+    {
+		//implement as described in the class diagram
+		
+        public CustomArgs(string before, string after)
+        {
+			//to be implemented
+        }
+    }
+}
--- Ex_39 ArchitectureUsingEvents/Program.cs
+++ Ex_39 ArchitectureUsingEvents/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ex_39_ArchitectureUsingEvents
+namespace ModelViewPresenter01
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Run();
+        }
+
+        public void Run()
+        {
+            Message message = new Message();
+            Screen screen = new Screen(message);
+            screen.ShowScreen();
         }
     }
 }
